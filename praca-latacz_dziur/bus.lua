27a7f467e7dd13b8852056322957c695ad257ff3
Deni2906 <166956645+Deni2906@users.noreply.github.com>
2024-04-13 23:39:16 +0200

    Add files via upload

---
+++ praca-latacz_dziur/bus.lua
@@ -0,0 +1,75 @@
+﻿--[[
+
+Author: Deni*
+--]]
+
+local b=createElement("text")
+setElementData(b,"name","PRACA: Naprawa Pasa Startowego\nZAROBEK:\n300PLN\nOPIS: Łatanie dziur")
+setElementPosition(b,-1328.74,-336.60,14.15)
+
+createBlip(-1327.56,-337.69,24.93, 46,2,0,0,0,0,0,275)
+
+local m1 = createMarker(-1328.74,-336.60,14.15-1, "cylinder", 1.5, 35, 142, 150)
+vehs = {}
+
+
+function removePreviousVehicles(plr)
+    for i,v in ipairs(getElementsByType("vehicle", resourceRoot)) do
+        local sby = getElementData(v, "zrespilGracz")
+        if sby and sby == plr and getPedOccupiedVehicle(plr) ~= v then
+            destroyElement(v)
+        end
+    end
+end
+
+addEventHandler("onMarkerHit", m1, function(el, md)
+    if getElementData(el, "zrespilGracz") then
+        --outputChatBox("Praca w trakcie przygotowania.", el)
+        return
+    end
+    
+    if not md or getElementType(el) ~= "player" or getPedOccupiedVehicle(el) then return end
+    
+    local x,y,z = getElementPosition(el)
+    local bus = createVehicle(485, -1652.04, -169.65, 13.89, 0.0, 0.0, 313.5)
+ 
+	setElementData(bus,"vehicle:desc","Praca naprawy pasa startowego \n! Proszę zachować ostrożność !")
+    setElementData(bus, "zrespilGracz", el)
+    setElementData(bus, "vehicle:fuel", 100)
+    setElementData(bus, "vehicle:mileage", math.random(10,30))
+	setVehiclePlateText(bus, " PRACA" )
+	setVehicleColor(bus, 0, 96, 255 )
+    
+    setVehicleHandling(bus,"maxVelocity", 100.00) -- 40 + 10 = 50km/h
+    
+    warpPedIntoVehicle(el, bus)
+    removePreviousVehicles(el)
+    triggerClientEvent(el, "STARTJobBus", resourceRoot, bus)
+    setVehicleHandling(bus,"maxVelocity", 100.00) -- 40 + 10 = 50km/h
+	setElementData(bus, "blokada:reczny_latdziur", true)
+
+    vehs[el] = bus
+end)
+
+addEvent("STOPJobBus", true)
+addEventHandler("STOPJobBus", resourceRoot, function()
+    local pojazd = getPedOccupiedVehicle(localPlayer)
+    if pojazd then
+        destroyElement(pojazd)
+    end
+end)
+
+addEvent("destroyVeh", true)
+addEventHandler("destroyVeh", getRootElement(),
+function()
+     if vehs[source] then
+          if isElement(vehs[source]) then destroyElement(vehs[source]) end
+     end
+end)
+
+addEvent("onPlayerQuit", true)
+addEventHandler ( "onPlayerQuit", getRootElement(), function()
+     if vehs[source] then
+          if isElement(vehs[source]) then destroyElement(vehs[source]) end
+    end
+end)
---
+++ praca-latacz_dziur/bus_c.lua
@@ -0,0 +1,140 @@
+﻿local districtsBus = {
+	-- TRASA SF --
+    {-1632.78, -149.59, 13.77},
+	{-1622.47, -118.26, 13.77},
+	{-1573.27, -86.56, 13.78},
+	{-1535.26, -78.05, 13.78},
+	{-1487.96, -79.14, 13.78},
+	{-1446.29, -53.60, 13.78},
+	{-1441.19, -3.57, 13.78},
+	{-1449.05, 29.65, 13.77},
+	{-1431.28, 50.50, 13.78},
+	{-1399.74, 84.50, 13.77},
+	{-1387.61, 114.24, 13.78},
+	{-1352.89, 129.59, 13.77},
+	{-1314.95, 190.72, 13.78},
+	{-1269.09, 199.42, 13.77},
+	{-1259.08, 139.53, 13.77},
+	{-1274.12, 100.64, 13.78},
+	{-1307.70, 37.84, 13.78},
+	{-1306.80, -15.36, 13.78},
+	{-1302.77, -73.23, 13.78},
+	{-1283.94, -125.54, 13.78},
+	{-1242.38, -168.73, 13.78},
+	{-1197.04, -233.50, 13.78},
+	{-1220.72, -262.33, 13.78},
+	{-1263.70, -280.32, 13.78},
+	{-1316.67, -295.14, 13.77},
+	-- TRASA LAS VENTURAS --
+}
+
+local jobTarget
+local jobMarker
+local jobVehicle
+local maxTarget = #districtsBus
+
+function finishJob()
+    if jobMarker and isElement(jobMarker) then
+        destroyElement(jobMarker)
+        jobMarker = nil
+    end
+    
+    if jobTarget and isElement(jobTarget) then
+        destroyElement(jobTarget)
+        jobTarget = nil
+        jobTarget = 0
+        
+    end
+    
+triggerServerEvent("destroyVeh", localPlayer)
+
+end
+
+
+function busDriver(el, md)
+    if el ~= localPlayer or not md then return end
+    if jobTarget > maxTarget and getPedOccupiedVehicle(el) then return end
+    if jobTarget == maxTarget and not getPedOccupiedVehicle(el) then return end
+    
+    if jobTarget == #districtsBus then
+        finishJob()
+        playSoundFrontEnd(5)
+        outputChatBox("* Zakończyłeś(aś) pracę.")
+		destroyElement(blip)
+		setElementData(el, "blokada:reczny_latdziur", false)
+    else
+        showMarker()
+        playSoundFrontEnd(12)
+		setTimer(function()
+        --triggerServerEvent("givePlayerMoney", localPlayer, 12, 0)
+		if getElementData(localPlayer,"player:premium") then
+		triggerServerEvent("givePlayerMoney", localPlayer, 350, 0)
+		setElementData(localPlayer,"player:srp", getElementData(localPlayer,"player:srp")+math.random(0,1))
+		else
+		triggerServerEvent("givePlayerMoney", localPlayer, 300, 0)
+		setElementData(localPlayer,"player:srp", getElementData(localPlayer,"player:srp")+math.random(0,1))
+		end
+		end, 8000, 1)
+    end
+end
+
+function showMarker()
+    if jobMarker and isElement(jobMarker) then
+		local veh=getPedOccupiedVehicle(localPlayer)
+		if not veh then return end
+		if getVehicleController(veh) ~= localPlayer then return end
+        --outputChatBox("* Załatałeś(aś) dziure. Jedź do kolejnej dziury.")
+        --destroyElement(jobMarker)
+        --jobMarker = nil
+		
+		outputChatBox("* Naprawiasz część lotniska, odczekaj 10 sekund.")
+		--destroyElement(blip)
+		setElementFrozen(veh, true)
+		setTimer(function()
+		setElementFrozen(veh, false)
+		outputChatBox("* Naprawiłeś część lotniska. Jedź do kolejnego miejsca.")
+        --destroyElement(jobMarker)
+        --jobMarker = nil
+		end, 8000, 1)
+		destroyElement(jobMarker)
+		destroyElement(blip)
+        jobMarker = nil
+		
+    end
+    
+    jobTarget = jobTarget + 1
+    jobMarker = createMarker(districtsBus[jobTarget][1], districtsBus[jobTarget][2], districtsBus[jobTarget][3], "checkpoint", 2.5, 255, 255, 255)
+    blip = createBlipAttachedTo(jobMarker, 41)
+	
+    if districtsBus[jobTarget+1] then
+        ile = districtsBus[jobTarget+1]
+        setMarkerTarget(jobMarker, ile[1], ile[2], ile[3])
+    end
+    
+    addEventHandler("onClientMarkerHit", jobMarker, busDriver)
+end
+
+addEvent("STARTJobBus", true)
+addEventHandler("STARTJobBus", resourceRoot, function(veh)
+    outputChatBox("* Rozpocząłeś(aś) pracę.")
+    jobVehicle = veh
+    jobTarget = 0
+    showMarker()
+end)
+
+addEventHandler("onClientResourceStop", resourceRoot, function()
+    if jobVehicle and getElementData(localPlayer, "player:job") then
+        setElementData(localPlayer, "player:job", false)
+    end
+end)
+
+addEventHandler("onClientVehicleExit", resourceRoot, function(plr, seat)
+    if seat == 0 then
+        if plr == localPlayer then
+            finishJob()
+			destroyElement(blip)
+            outputChatBox("#51c26f>> #ffffffZakończyłeś pracę.",255,255,255,true)
+			setElementPosition(localPlayer,-1324.75, -334.73, 14.15)
+        end
+    end
+end)
